e5d248ade78cfd9981c06d23708a726cd1b478da
omarASC5 <ocobas19@avenues.org>
2019-02-15 21:01:51 -0500

    setup up a simple neural network with Brain.js

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require("express"),
 		app = express(),
 	  newsAPI = require("newsapi"),
-	  ejs     = require("ejs"),
-	  keys    = require("./config/keys.js"),
-	  newsapi = new newsAPI(keys.NEWS_API.KEY);
+		ejs     = require("ejs"),
+		keys    = require("./config/keys.js"),
+		compromise = require("compromise"),
+		brain = require("brain.js"),
+		newsapi = new newsAPI(keys.NEWS_API.KEY);
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
+// const doc = nlp('dinosaur');
+// doc.nouns().toPlural();
+// console.log(doc.out('text'));
 
-function GetFormattedDate() {
-    var todayTime = new Date();
-    var month = format(todayTime .getMonth() + 1);
-    var day = format(todayTime .getDate());
-    var year = format(todayTime .getFullYear());
-    return month + "/" + day + "/" + year;
-}
+const network = new brain.NeuralNetwork();
+network.train([
+	{ input: [0, 0, 0], output: [0] },
+	{ input: [0, 0, 1], output: [0] },
+	{ input: [0, 1, 1], output: [0] },
+	{ input: [1, 0, 1], output: [1] },
+	{ input: [1, 1, 1], output: [1] }
+]);
+
+const output = network.run([0, 0, 1]);
+
+console.log(`Probability: ${output}`);
+
 // To query /v2/everything
 // You must include at least one q, source, or domain
 newsapi.v2.everything({
